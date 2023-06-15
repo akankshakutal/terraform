320bdf054b6975d1ac92efe6d712f5419f5a7068
akanksha kutal <akankshak@sahaj.ai>
2023-06-15 12:45:42 +0530

    Add public and private subnet

--- main.tf
+++ main.tf
@@ -10,14 +10,36 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-south-1"
+  region = "ap-south-1"
+}
+
+locals {
+  common_tags = {
+    creatorName = "akanksha Kutal"
+    reason      = "CapDev infra"
+  }
+}
+
+resource "aws_vpc" "capDev_infra" {
+  cidr_block = "11.0.0.0/23"
+  tags       = local.common_tags
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.capDev_infra.id
+  cidr_block = "11.0.0.0/24"
+  tags       = local.common_tags
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.capDev_infra.id
+  cidr_block = "11.0.1.0/24"
+  tags       = local.common_tags
 }
 
 resource "aws_instance" "nginx_server" {
   ami           = "ami-057752b3f1d6c4d6c"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "nginx-server"
-  }
+  subnet_id     = aws_subnet.private_subnet.id
+  tags          = local.common_tags
 }
